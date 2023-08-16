49a2590ae43d423c52b01d9810dd682c0b681bec
EdiJunior88 <er.moura88@yahoo.com.br>
2023-08-16 10:33:50 -0300

    10 Projeto Final 1001 Análise Projeto 1002 Estrutura Inicial 1003 Repositório Git 1004 Header e Menu 1 1004 Header e Menu 2 1005 Introdução 1 1005 Introdução 2 1006 Box Shadow 1007 Introdução Responsiva 1008 Tipografia 1009 Import CSS 1010 CSS Utilitário 1011 Tipografia Utilitária 1 1011 Tipografia Utilitária 2 1012 Bicicletas Lista 1 1012 Bicicletas Lista 2 1013 Container 1014 Tecnologia 1 1014 Tecnologia 2 1015 Parceiros 1016 Depoimento 1017 Seguros 1018 Footer 1019 Termos 1020 Bicicletas 1 1020 Bicicletas 2 1020 Bicicletas 3 1021 Seguros 1 1021 Seguros 2 1022 Bicicleta Interna 1 1022 Bicicleta Interna 2 1023 Bicicleta Outras 1024 Bicicleta Seguro 1025 Contato Dados 1 1025 Contato Dados 2 1026 Contato Formulário 1027 Contato Lojas 1028 Orçamento 1 1028 Orçamento 2 1028 Orçamento 3 1028 Orçamento 4 1028 Orçamento 5 1029 Decoração 1030 Otimizações
    11 JavaScript Básico
    1101 JavaScript
    1102 Manipular DOM
    1103 Tipos de Dados
    1104 Objetos
    1105 Funções 1
    1105 Funções 2
    1106 Eventos 1
    1106 Eventos 2
    1106 Eventos 3
    1107 Condicionais 1
    1107 Condicionais 2
    1108 Arrays e Loops 1
    1108 Arrays e Loops 2
    1108 Arrays e Loops 3
    1109 Solucionar Problemas
    
    12 JavaScript Projeto
    1201 Menu Ativo
    1202 Parâmetros URL
    1203 Perguntas Frequentes
    1204 Galeria Imagens
    1205 Plugin Animar
    1206 Plugin Externo
    1207 JavaScript Desativado
    
    13 Produção
    1301 Otimizar Produção
    1302 Hospedagem 1
    1302 Hospedagem 2
    1303 Acessar Servidor
    1304 Formulário PHP
    1305 Formulário JavaScript

---
+++ bikcraft/js/formulario.js
@@ -0,0 +1,27 @@
+const formulario = document.querySelector("form");
+
+function formularioEnviado(resposta) {
+  if (resposta.ok) {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317A00;'>Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em 24 horas.</p>";
+  } else {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #E00000;'>Erro no envio</span>, você pode enviar diretamente para o nosso email em: contato@bikcraft.net</p>";
+  }
+}
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector("form button");
+  botao.disabled = true;
+  botao.innerText = "Enviando...";
+
+  const data = new FormData(formulario);
+
+  fetch("./enviar.php", {
+    method: "POST",
+    body: data,
+  }).then(formularioEnviado);
+}
+
+formulario.addEventListener("submit", enviarFormulario);
---
+++ bikcraft/js/script.js
@@ -0,0 +1,67 @@
+// Ativar Links do Menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
